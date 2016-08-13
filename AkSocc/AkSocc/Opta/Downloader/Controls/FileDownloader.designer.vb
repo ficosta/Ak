5846c311182bc0409153b8f54f33e16749464494
lontano <gelo_oleg@hotmail.com>
2016-08-13 06:13:00 +0200

    nightly 13/08/2016

---
+++ AkSocc/AkSocc/Opta/Downloader/Controls/FileDownloader.designer.vb
@@ -0,0 +1,93 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class FileDownloader
+  Inherits System.Windows.Forms.UserControl
+
+  'UserControl overrides dispose to clean up the component list.
+  <System.Diagnostics.DebuggerNonUserCode()> _
+  Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+    Try
+      If disposing AndAlso components IsNot Nothing Then
+        components.Dispose()
+      End If
+    Finally
+      MyBase.Dispose(disposing)
+    End Try
+  End Sub
+
+  'Required by the Windows Form Designer
+  Private components As System.ComponentModel.IContainer
+
+  'NOTE: The following procedure is required by the Windows Form Designer
+  'It can be modified using the Windows Form Designer.  
+  'Do not modify it using the code editor.
+  <System.Diagnostics.DebuggerStepThrough()> _
+  Private Sub InitializeComponent()
+    Me.TableLayoutPanel1 = New System.Windows.Forms.TableLayoutPanel()
+    Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
+    Me.LabelState = New System.Windows.Forms.Label()
+    Me.MetroProgressBar1 = New MetroFramework.Controls.MetroProgressBar()
+    Me.TableLayoutPanel1.SuspendLayout()
+    Me.SuspendLayout()
+    '
+    'TableLayoutPanel1
+    '
+    Me.TableLayoutPanel1.ColumnCount = 2
+    Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100.0!))
+    Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 150.0!))
+    Me.TableLayoutPanel1.Controls.Add(Me.ProgressBar1, 0, 0)
+    Me.TableLayoutPanel1.Controls.Add(Me.LabelState, 1, 0)
+    Me.TableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill
+    Me.TableLayoutPanel1.Location = New System.Drawing.Point(0, 0)
+    Me.TableLayoutPanel1.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+    Me.TableLayoutPanel1.Name = "TableLayoutPanel1"
+    Me.TableLayoutPanel1.RowCount = 1
+    Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100.0!))
+    Me.TableLayoutPanel1.Size = New System.Drawing.Size(200, 50)
+    Me.TableLayoutPanel1.TabIndex = 0
+    '
+    'ProgressBar1
+    '
+    Me.ProgressBar1.Dock = System.Windows.Forms.DockStyle.Fill
+    Me.ProgressBar1.Location = New System.Drawing.Point(3, 4)
+    Me.ProgressBar1.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+    Me.ProgressBar1.Name = "ProgressBar1"
+    Me.ProgressBar1.Size = New System.Drawing.Size(44, 42)
+    Me.ProgressBar1.TabIndex = 0
+    '
+    'LabelState
+    '
+    Me.LabelState.Dock = System.Windows.Forms.DockStyle.Fill
+    Me.LabelState.Font = New System.Drawing.Font("Segoe UI", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+    Me.LabelState.Location = New System.Drawing.Point(53, 0)
+    Me.LabelState.Name = "LabelState"
+    Me.LabelState.Size = New System.Drawing.Size(144, 50)
+    Me.LabelState.TabIndex = 1
+    Me.LabelState.Text = "State"
+    Me.LabelState.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+    '
+    'MetroProgressBar1
+    '
+    '    Me.MetroProgressBar1.BackColor = System.Drawing.Color.DimGray
+    Me.MetroProgressBar1.Location = New System.Drawing.Point(0, 0)
+    Me.MetroProgressBar1.Name = "MetroProgressBar1"
+    Me.MetroProgressBar1.TabIndex = 0
+    '
+    'FileDownloader
+    '
+    Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 17.0!)
+    Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+    Me.Controls.Add(Me.TableLayoutPanel1)
+    Me.DoubleBuffered = True
+    Me.Font = New System.Drawing.Font("Segoe UI", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+    Me.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+    Me.Name = "FileDownloader"
+    Me.Size = New System.Drawing.Size(200, 50)
+    Me.TableLayoutPanel1.ResumeLayout(False)
+    Me.ResumeLayout(False)
+
+  End Sub
+  Friend WithEvents TableLayoutPanel1 As TableLayoutPanel
+  Friend WithEvents ProgressBar1 As ProgressBar
+  Friend WithEvents LabelState As Label
+  Private WithEvents MetroProgressBar1 As MetroFramework.Controls.MetroProgressBar
+End Class
---
+++ AkSocc/AkSocc/Opta/Downloader/Controls/FileDownloader.vb
@@ -0,0 +1,105 @@
+﻿Imports System.ComponentModel
+Imports System.Net
+
+
+Public Class FileDownloader
+  Implements INotifyPropertyChanged
+
+  Private WithEvents _client As WebClient
+
+  Public Event FileProgressChanged(sender As FileDownloader, download As Download)
+  Public Event FileDownloaded(sender As FileDownloader, download As Download)
+  Public Event PropertyChanged As PropertyChangedEventHandler Implements INotifyPropertyChanged.PropertyChanged
+  Public ReadOnly Property Download As Download
+
+  Private _isBusy As Boolean = False
+  Public Property IsBusy As Boolean
+    Get
+      Return _isBusy
+    End Get
+    Set(value As Boolean)
+      _isBusy = value
+    End Set
+  End Property
+
+  Public ReadOnly Property Progress As Integer
+    Get
+      If _Download Is Nothing Then
+        Return 0
+      Else
+        Return _Download.Progress
+      End If
+    End Get
+  End Property
+
+
+  Private Sub FileDownloader_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+  End Sub
+
+  Public Sub StartDownload(down As Download)
+    Try
+      If IsBusy Then Exit Sub
+      If down Is Nothing Then Exit Sub
+
+      _Download = down
+
+      If Not _client Is Nothing Then
+        'we are not done!
+      Else
+        _client = New WebClient
+      End If
+      IsBusy = True
+
+      _client.Credentials = New System.Net.NetworkCredential(down.User, down.Password)
+
+      _client.DownloadFileAsync(New Uri(down.URL), down.File)
+      _Download.Progress = 0
+      _Download.State = Download.eDownloadState.BeginingDownload
+      Me.LabelState.Text = "Start downloading"
+    Catch ex As Exception
+      _Download.ErrorCode = ex.HResult
+      _Download.State = Download.eDownloadState.Error
+    End Try
+  End Sub
+
+
+  Private Sub _client_DownloadProgressChanged(sender As Object, e As DownloadProgressChangedEventArgs) Handles _client.DownloadProgressChanged
+    'report progress
+
+    Dim bytesIn As Double = Double.Parse(e.BytesReceived.ToString())
+
+    Dim totalBytes As Double = Double.Parse(e.TotalBytesToReceive.ToString())
+
+    Dim percentage As Double = bytesIn / totalBytes * 100
+
+    _Download.Progress = Int32.Parse(Math.Truncate(percentage).ToString())
+
+    Me.LabelState.Text = _Download.Progress & "%"
+
+    Me.ProgressBar1.Value = Int32.Parse(Math.Truncate(percentage).ToString())
+
+    RaiseEvent FileProgressChanged(Me, _Download)
+    RaiseEvent PropertyChanged(Me, New PropertyChangedEventArgs("Progress"))
+  End Sub
+
+  Private Sub _client_DownloadStringCompleted(sender As Object, e As DownloadStringCompletedEventArgs) Handles _client.DownloadStringCompleted
+    'report complete
+    IsBusy = False
+    _Download.Progress = 100
+    _Download.State = Download.eDownloadState.Done
+    Me.LabelState.Text = "_client_DownloadStringCompleted"
+    RaiseEvent FileDownloaded(Me, _Download)
+    RaiseEvent PropertyChanged(Me, New PropertyChangedEventArgs("Progress"))
+  End Sub
+
+  Private Sub _client_DownloadFileCompleted(sender As Object, e As AsyncCompletedEventArgs) Handles _client.DownloadFileCompleted
+    'report complete
+    IsBusy = False
+    _Download.Progress = 100
+    _Download.State = Download.eDownloadState.Done
+    RaiseEvent FileDownloaded(Me, _Download)
+    RaiseEvent PropertyChanged(Me, New PropertyChangedEventArgs("Progress"))
+    Me.LabelState.Text = "_client_DownloadFileCompleted"
+  End Sub
+End Class
