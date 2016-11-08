01a9492d8f8658c9dcedc6d92e972e3387d15656
lontano <gelo_oleg@hotmail.com>
2016-11-08 20:45:53 +0100

    formScript

---
+++ AkSocc/VizCommands/FormScript.Designer.vb
@@ -0,0 +1,69 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class FormScript
+  Inherits System.Windows.Forms.Form
+
+  'Form overrides dispose to clean up the component list.
+  <System.Diagnostics.DebuggerNonUserCode()> _
+  Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+    Try
+      If disposing AndAlso components IsNot Nothing Then
+        components.Dispose()
+      End If
+    Finally
+      MyBase.Dispose(disposing)
+    End Try
+  End Sub
+
+  'Required by the Windows Form Designer
+  Private components As System.ComponentModel.IContainer
+
+  'NOTE: The following procedure is required by the Windows Form Designer
+  'It can be modified using the Windows Form Designer.  
+  'Do not modify it using the code editor.
+  <System.Diagnostics.DebuggerStepThrough()> _
+  Private Sub InitializeComponent()
+    Me.Scintilla1 = New ScintillaNET.Scintilla()
+    Me.TableLayoutPanel1 = New System.Windows.Forms.TableLayoutPanel()
+    Me.TableLayoutPanel1.SuspendLayout()
+    Me.SuspendLayout()
+    '
+    'Scintilla1
+    '
+    Me.Scintilla1.Dock = System.Windows.Forms.DockStyle.Fill
+    Me.Scintilla1.Location = New System.Drawing.Point(33, 3)
+    Me.Scintilla1.Name = "Scintilla1"
+    Me.Scintilla1.Size = New System.Drawing.Size(838, 439)
+    Me.Scintilla1.TabIndex = 0
+    Me.Scintilla1.Text = "Scintilla1"
+    '
+    'TableLayoutPanel1
+    '
+    Me.TableLayoutPanel1.ColumnCount = 2
+    Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 30.0!))
+    Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100.0!))
+    Me.TableLayoutPanel1.Controls.Add(Me.Scintilla1, 1, 0)
+    Me.TableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill
+    Me.TableLayoutPanel1.Location = New System.Drawing.Point(0, 0)
+    Me.TableLayoutPanel1.Name = "TableLayoutPanel1"
+    Me.TableLayoutPanel1.RowCount = 2
+    Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100.0!))
+    Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30.0!))
+    Me.TableLayoutPanel1.Size = New System.Drawing.Size(874, 475)
+    Me.TableLayoutPanel1.TabIndex = 1
+    '
+    'FormScript
+    '
+    Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+    Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+    Me.ClientSize = New System.Drawing.Size(874, 475)
+    Me.Controls.Add(Me.TableLayoutPanel1)
+    Me.Name = "FormScript"
+    Me.Text = "FormScript"
+    Me.TableLayoutPanel1.ResumeLayout(False)
+    Me.ResumeLayout(False)
+
+  End Sub
+
+  Friend WithEvents Scintilla1 As ScintillaNET.Scintilla
+  Friend WithEvents TableLayoutPanel1 As System.Windows.Forms.TableLayoutPanel
+End Class
